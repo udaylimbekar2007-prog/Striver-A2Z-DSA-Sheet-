88db8177875f210f3be83765fcbfb02c7e1e7932
Uday Prakash Limbekar <uday.limbekar2007@gmail.com>
2026-07-27 08:08:00 +0530

    Update pattern5.cpp

--- pattern5.cpp
+++ pattern5.cpp
@@ -36,26 +36,3 @@ int main() {
 	Solution solutions;
 	solutions.pattern5(n);
 }
-
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
-	public:
-		void pattern5(int n) {
-			for(int i = 0; i < n; i++) {
-				for(int j = 1; j <= n-i; j++) {
-					cout << "*";
-				}
-				cout << endl;
-			}
-		}
-};
-
-int main() {
-	int n;
-	cout << "enter the value of n: ";
-	cin >> n;
-	Solution solutions;
-	solutions.pattern5(n);
-}
