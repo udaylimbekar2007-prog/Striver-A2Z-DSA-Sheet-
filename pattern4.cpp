1546c95ac68d137b26d2bfa47258bbe76b4ea1e2
Uday Prakash Limbekar <uday.limbekar2007@gmail.com>
2026-07-27 08:06:35 +0530

    Update pattern4.cpp

--- pattern4.cpp
+++ pattern4.cpp
@@ -1,3 +1,20 @@
+/* problem statement : 
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+
+1
+
+22
+
+333
+
+4444
+
+55555
+
+
+Print the pattern in the function given to you. */
+
 #include<bits/stdc++.h>
 using namespace std;
 
