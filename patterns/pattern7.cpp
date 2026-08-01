4729ad0d9ea6e813cd77a39c01c9a93b19f6e84b
Uday Prakash Limbekar <uday.limbekar2007@gmail.com>
2026-08-01 20:19:13 +0530

    Add files via upload

---
+++ patterns/pattern7.cpp
@@ -0,0 +1,41 @@
+/* Given an integer n. You need to recreate the pattern given below for any value of N. 
+Let's say for N = 5, the pattern should look like as below:
+
+
+    *
+   ***
+  *****
+ *******
+*********
+
+
+Print the pattern in the function given to you. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+		void pattern7(int n) {
+			for(int i = 0; i < n; i++) {
+				for(int j = 1; j <=	n-i-1; j++) {
+					cout << " ";
+				}
+				for(int j = 1; j <= 2*i+1; j++) {
+					cout << "*";
+				}
+				for(int j = 1; j <=	n-i-1; j++) {
+				cout << " ";
+				}
+				cout << endl;	
+			}
+		}
+};
+
+int main() {
+	Solution solutions;
+	int n;
+	cout << "Enter the value of n: ";
+	cin >> n;solutions.pattern7(n);
+	return 0;
+}
