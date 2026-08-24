8721a968638bac154d987d7333d7d979fa193f7e
Uday Prakash Limbekar <uday.limbekar2007@gmail.com>
2026-08-24 09:39:40 +0530

    Add files via upload

---
+++ patterns/pattern8.cpp
@@ -0,0 +1,39 @@
+/* Given an integer n. You need to recreate the pattern given below for any value of N.
+Let's say for N = 5, the pattern should look like as below:
+
+
+*********
+ *******
+  *****
+   ***
+    *
+
+
+Print the pattern in the function given to you. */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+		void pattern8(int n) {
+			for(int i = 0; i < n; i++) {
+			for(int j = 0; j < i; j++) {
+				cout << " ";
+			}
+			for(int j = 0; j < 2*n - (2*i + 1); j++) {
+				cout << "*";
+			}
+			cout << endl;
+		}
+	}
+};
+
+int main() {
+	Solution solutions;
+	int n;
+	cout << "enter the value of n: ";
+	cin >> n;
+	solutions.pattern8(n);
+	return 0;
+}
